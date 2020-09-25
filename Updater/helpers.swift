d0053b61dacb1226642eaf20add311ae7fcb7f47
Serhiy Mytrovtsiy <mitrovtsiy@ukr.net>
2020-09-26 00:04:32 +0200

    - initialize the framework

---
+++ Updater/helpers.swift
@@ -0,0 +1,77 @@
+//
+//  helpers.swift
+//  Updater
+//
+//  Created by Serhiy Mytrovtsiy on 25/09/2020.
+//  Using Swift 5.0.
+//  Running on macOS 10.15.
+//
+//  Copyright © 2020 Serhiy Mytrovtsiy. All rights reserved.
+//
+
+import Foundation
+import SystemConfiguration
+
+internal extension URL {
+    func checkFileExist() -> Bool {
+        return FileManager.default.fileExists(atPath: self.path)
+    }
+}
+
+extension String: LocalizedError {
+    public var errorDescription: String? { return self }
+    
+    func condenseWhitespace() -> String {
+        let components = self.components(separatedBy: .whitespacesAndNewlines)
+        return components.filter { !$0.isEmpty }.joined(separator: " ")
+    }
+}
+
+internal func asyncShell(_ args: String) {
+    let task = Process()
+    task.launchPath = "/bin/sh"
+    task.arguments = ["-c", args]
+    let pipe = Pipe()
+    task.standardOutput = pipe
+    task.launch()
+}
+
+internal func syncShell(_ args: String) -> String {
+    let task = Process()
+    task.launchPath = "/bin/sh"
+    task.arguments = ["-c", args]
+    let pipe = Pipe()
+    
+    task.standardOutput = pipe
+    task.launch()
+    task.waitUntilExit()
+    
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output = String(data: data, encoding: .utf8)!
+    
+    return output
+}
+
+// https://stackoverflow.com/questions/30743408/check-for-internet-connection-with-swift
+internal func isConnectedToNetwork() -> Bool {
+    var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
+    zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+    zeroAddress.sin_family = sa_family_t(AF_INET)
+    
+    let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+        $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+            SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+        }
+    }
+    
+    var flags: SCNetworkReachabilityFlags = SCNetworkReachabilityFlags(rawValue: 0)
+    if SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) == false {
+        return false
+    }
+    
+    let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+    let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+    let ret = (isReachable && !needsConnection)
+    
+    return ret
+}
